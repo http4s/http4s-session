d2e05ce49a662ef8cf12a73bd20a1683fac03b9c
danicheg <e.danicheg@yandex.ru>
2021-09-12 00:44:47 +0300

    Format

--- core/src/main/scala/org/http4s/session/SessionIdentifier.scala
+++ core/src/main/scala/org/http4s/session/SessionIdentifier.scala
@@ -11,22 +11,27 @@ object SessionIdentifier {
   private val b64 = Base64.getEncoder()
 
   /**
-    * Generator for a SecureIdentifier
-    *
-    * @param random Use Something backed by SecureRandom or a CSPRNG source
-    * @param numBytes Minimum recommended by OWASP is 16 bytes if you have 64 bits of entropy
-    * @return A Session Identifier
-    */
+   * Generator for a SecureIdentifier
+   *
+   * @param random
+   *   Use Something backed by SecureRandom or a CSPRNG source
+   * @param numBytes
+   *   Minimum recommended by OWASP is 16 bytes if you have 64 bits of entropy
+   * @return
+   *   A Session Identifier
+   */
   def create[F[_]: Functor](random: Random[F], numBytes: Int): F[SessionIdentifier] = {
     random.nextBytes(numBytes).map(a => SessionIdentifier(b64.encodeToString(a)))
   }
 
   /**
-    * Convenience Method From Extracting a Session from a Request
-    *
-    * @param req Request to extract from
-    * @param sessionIdentifierName The Name Of The Session Identifier - A.k.a Which cookie its in
-    */
+   * Convenience Method From Extracting a Session from a Request
+   *
+   * @param req
+   *   Request to extract from
+   * @param sessionIdentifierName
+   *   The Name Of The Session Identifier - A.k.a Which cookie its in
+   */
   def extract[F[_]](req: Request[F], sessionIdentifierName: String): Option[SessionIdentifier] = {
     req.cookies.find(_.name === sessionIdentifierName).map(c => SessionIdentifier(c.content))
   }
